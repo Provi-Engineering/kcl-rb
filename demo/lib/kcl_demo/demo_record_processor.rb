2d642820862d17848f33816dfa88699477a9be10
Jeffrey Lee <jeff@sevenfifty.com>
2022-05-11 12:24:44 -0400

    Update log messages from info to debug

--- demo/lib/kcl_demo/demo_record_processor.rb
+++ demo/lib/kcl_demo/demo_record_processor.rb
@@ -4,12 +4,12 @@ module KclDemo
   class DemoRecordProcessor < Kcl::RecordProcessor
     # @implement
     def after_initialize(initialization_input)
-      Kcl.logger.info("Initialization at #{initialization_input}")
+      Kcl.logger.debug("Initialization at #{initialization_input}")
     end
 
     # @implement
     def process_records(records_input)
-      Kcl.logger.info('Processing records...')
+      Kcl.logger.debug('Processing records...')
 
       # レコードのリストを取得
       return if records_input.records.empty?
@@ -19,12 +19,12 @@ module KclDemo
       # rubocop:enable Lint/Debugger
 
       records_input.records.each do |record|
-        Kcl.logger.info("Record = #{record}")
+        Kcl.logger.debug("Record = #{record}")
       end
 
       # チェックポイントを記録
       last_sequence_number = records_input.records[-1].sequence_number
-      Kcl.logger.info(
+      Kcl.logger.debug(
         "Checkpoint progress at: #{last_sequence_number}" \
         ", MillisBehindLatest = #{records_input.millis_behind_latest}"
       )
@@ -33,7 +33,7 @@ module KclDemo
 
     # @implement
     def shutdown(shutdown_input)
-      Kcl.logger.info("Shutdown reason: #{shutdown_input.shutdown_reason}")
+      Kcl.logger.debug("Shutdown reason: #{shutdown_input.shutdown_reason}")
 
       if shutdown_input.shutdown_reason == Kcl::Workers::ShutdownReason::TERMINATE
         shutdown_input.record_checkpointer.update_checkpoint(nil)
